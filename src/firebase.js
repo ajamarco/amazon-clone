a147d25e705c89f6ecffce3b4ef52fa4cdef22a2
alexandre jamarco <alexandre.j@gmail.com>
2021-11-12 08:47:39 +0000

    created firebase files

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3bgnviDgnC7JvBlti7BhyE4H6ZWiC-xQ",
+    authDomain: "clone-63619.firebaseapp.com",
+    projectId: "clone-63619",
+    storageBucket: "clone-63619.appspot.com",
+    messagingSenderId: "724124615785",
+    appId: "1:724124615785:web:bb392e99afcabb88a48ef8",
+    measurementId: "G-WR87MHR8JT"
+  };
